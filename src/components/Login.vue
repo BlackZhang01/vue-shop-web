bb93a6328cea4422b2beae24c4f4de64ae6be749
BlackZhang01 <zhyong138@qq.com>
2021-11-30 06:45:00 +0800

    login background

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Login component</h1>
+  <div class="login_container">
+    <div class="login_box"></div>
   </div>
 </template>
 
@@ -11,4 +11,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
