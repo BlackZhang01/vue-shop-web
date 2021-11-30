345b1332c5ea0172dc9620e9502def0cbd25c4e0
BlackZhang01 <zhyong138@qq.com>
2021-11-30 22:28:31 +0800

    add router 导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,20 @@ import Home from '../components/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     routes: [
         { path: '/', redirect: '/login' },
         { path: '/login', component: Login },
         { path: '/home', component: Home }
     ]
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') return next();
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login');
+    next();
+
+})
+
+export default router
