8edb1bd411ac8d1dfdfd00ee74c3a442df2ef17f
BlackZhang01 <zhyong138@qq.com>
2021-11-30 12:29:37 +0800

    login add model rules

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,16 +5,21 @@
         <img src="../assets/logo.png" alt="" />
       </div>
       <!-- form表单 -->
-      <el-form :model="loginForm" label-width="0px" class="login_form">
+      <el-form
+        :rules="loginFormRules"
+        :model="loginForm"
+        label-width="0px"
+        class="login_form"
+      >
         <!-- 用户名 -->
-        <el-form-item>
+        <el-form-item prop="username">
           <el-input
             v-model="loginForm.username"
             prefix-icon="el-icon-user"
           ></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input
             type="password"
             v-model="loginForm.password"
@@ -40,8 +45,28 @@ export default {
   data() {
     return {
       loginForm: {
-        username: "zs",
-        password: "123",
+        username: "",
+        password: "",
+      },
+      loginFormRules: {
+        username: [
+          {
+            min: 3,
+            max: 10,
+            message: "请输入 3 到 10 位字符",
+            trigger: "blur",
+          },
+          { required: true, message: "请输入用户名", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
       },
     };
   },
